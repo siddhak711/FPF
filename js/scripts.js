d15919d5eb19ff2d430fc7b5d8cbe1b19ec73a33
Siddha Kanthi <siddhakanthi@gmail.com>
2024-07-28 16:44:05 -0500

    Merge remote changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document is ready');
-<<<<<<< HEAD
     // Add your JavaScript code here if needed
-=======
-    // Add your JavaScript code here
->>>>>>> 5754e8da9b86cff47be2006c01d99fc408c8b08e
 });
